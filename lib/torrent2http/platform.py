fdac53894006fbede9359777d06db15e895e3fed
DiMartinoXBMC <reportonme@mail.ru>
2016-12-23 00:35:32 +0300

    aarch64 fix by vkravets

--- lib/torrent2http/platform.py
+++ lib/torrent2http/platform.py
@@ -19,7 +19,7 @@ class Platform:
                                                          uname()[4].lower().startswith('aarch')):
             if uname()[4].lower().startswith('armv6'):
                 return "armv6"
-            elif uname()[4].lower().startswith('aarch64'):
+            elif uname()[4].lower().startswith('aarch64') and sys.maxsize > 2 ** 32:
                 return "aarch64"
             return 'arm'
         elif sys.maxsize > 2**32:
